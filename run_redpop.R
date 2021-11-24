5f98637d002c149f6eced168158d28decf69a2d7
Adam Dinan <ad866@login-l-1.data.cluster>
2021-11-24 09:25:10 +0000

    make chromosome selection optional

--- run_redpop.R
+++ run_redpop.R
@@ -12,18 +12,18 @@ args = commandArgs(trailingOnly=TRUE)
 ## 4. bwExt = File extension for BigWigs
 ## 5. chr_select = Selected chromosome to scan
 
-if (!length(args)<4) {
-    stop("Must supply at least 4 arguments")
-} else {
-    in_df = args[1]
-    atacDatDir = args[2]
-    chipDatDir = args[3]
-    bwExt = args[4]
-    chr_select = args[5]
-}
+
+
+in_df = args[1]
+atacDatDir = args[2]
+chipDatDir = args[3]
+bwExt = args[4]
+
+if(exists(args[5])) chr_select = args[5]
 
 source("redpop_v2.R")
 source("redpop_helpers.R")
+
 
 
 ##------------------------------------------------------------------------------
@@ -48,10 +48,12 @@ chipF <- paste0(chipDatDir, "/", chip_atac_df[["Chip_sample"]], bwExt)
 lapply(1:nrow(chip_atac_df), function(x){
     ## subset to OCRs in this sample (one per chromosome)
     sample_OCRs <- fseqOCRs[chip_atac_df[x,][["ATAC_sample"]]][[1]]
-    sample_OCRs <- subset(sample_OCRs, names(sample_OCRs)==chr_select)
+    if(exists(args[5])){
+        sample_OCRs <- subset(sample_OCRs, names(sample_OCRs)==chr_select)
+    }
     atac_file <- atacF[x]
     chip_file <- chipF[x]
-    ## idenitifier for the sample:
+    ## idenitifier for the sample
     sample <- paste0(chip_atac_df$Individual,"_",chip_atac_df$Time)[x]
     if(length(sample_OCRs)>1){
         lapply(seq_along(sample_OCRs), function(chr_idx){
